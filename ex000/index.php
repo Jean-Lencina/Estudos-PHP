0aedc46a86ad676f270f58d994f12769eabbc209
Jean Lencina <jclencina55@gmail.com>
2023-02-04 09:26:07 -0300

    Iniciei os estudos de PHP

---
+++ ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu primeiro código em PHP</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            print 'Olá mundo';
+        ?>
+    </h1>
+    <p>Me livrando da maldição</p>
+</body>
+</html>
